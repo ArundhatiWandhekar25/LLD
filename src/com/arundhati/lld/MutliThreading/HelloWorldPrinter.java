4ce77d4b46812058775e2e1d372ac51cf0154cbb
Arundhati Wandhekar <arundhati.wandhekar@sincrodigital.com>
2026-03-15 12:48:58 +0530

    how threads executes

--- src/com/arundhati/lld/MutliThreading/HelloWorldPrinter.java
+++ src/com/arundhati/lld/MutliThreading/HelloWorldPrinter.java
@@ -1,8 +1,15 @@
 package com.arundhati.lld.MutliThreading;
 
+import javax.swing.plaf.TableHeaderUI;
+
 public class HelloWorldPrinter implements Runnable{
+
+    void doSomething(){
+        System.out.println("Hello World From DoSomething " + Thread.currentThread().getName());
+    }
     @Override
     public void run() {
         System.out.println("Hello Wolrd " + Thread.currentThread().getName());
+        doSomething();
     }
 }
--- src/com/arundhati/lld/MutliThreading/Main.java
+++ src/com/arundhati/lld/MutliThreading/Main.java
@@ -2,10 +2,13 @@ package com.arundhati.lld.MutliThreading;
 
 public class Main {
     public static void main(String []args){
-        System.out.println("Hello World " + Thread.currentThread().getName());
-
+       int a =10;
+       int b= 20;
         HelloWorldPrinter tmp= new HelloWorldPrinter();
         Thread th= new Thread(tmp);
         th.start();
+
+        System.out.println("Hello World " + Thread.currentThread().getName());
+
     }
 }
